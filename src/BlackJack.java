388626134fc85415890d15e1eb646f083d51defb
WHOmagoo <hrmagoo@gmail.com>
2015-05-12 10:42:15 -0700

    Update BlackJack.java

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -1,15 +1,5 @@
-import java.util.Arrays;
-
-/**
- * Created by mphannon on 5/9/15.
- */
-
-//This should be under a gamemode class or interface
-public class BlackJack {
-    private Deck deck;
-	public BlackJack(Deck deck) {
-		this.deck = deck;
-		//Card[] shuffled = deck.shuffle();
-		//System.out.println(Arrays.toString(deck.deckOfCards));
-	}
+public class Main{
+    public static void main(String[] args){
+        
+    }
 }
