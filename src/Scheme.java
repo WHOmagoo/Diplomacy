fa3100b97b85ab069374fde51c54a0d9c94de749
Hugh McGough <hrmagoo@gmail.com>
2015-05-14 11:48:29 -0700

    Fixed up the Scheme enum and changed the information class for readability

--- src/Scheme.java
+++ src/Scheme.java
@@ -3,10 +3,22 @@ import java.awt.*;
 public enum Scheme {
     FOREGROUND (Color.BLUE), BACKGROUND(Color.ORANGE), FONT(new Font("Dialog", Font.BOLD, 12));
 
+    Color color;
+    Font font;
+
     Scheme(Color color) {
+        this.color = color;
     }
 
     Scheme(Font font){
+        this.font = font;
+    }
 
+    Color getColor(){
+        return color;
+    }
+
+    Font getFont(){
+        return font;
     }
 }
