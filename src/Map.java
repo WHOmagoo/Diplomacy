b3e83a6a9d88fb9c9fcad5dce91412925a72f9da
WHOmagoo? <hrmagoo@gmail.com>
2015-05-16 14:37:14 -0700

    Added a verify method to double check that all the borders in the countries are consistent. The map class now extends JPanel instead of an ArrayList for greater usability. (never used the super class except to create an arrayList)

--- src/Map.java
+++ src/Map.java
@@ -1,22 +1,24 @@
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Map extends ArrayList<Country> {
+public class Map extends JPanel {
+    ArrayList<Country> countries = new ArrayList<Country>();
 
     public Map(ArrayList<Country> countries) {
-        super.addAll(countries);
-        Collections.sort(this);
+        this.countries.addAll(countries);
+        Collections.sort(this.countries);
     }
 
     public void calculateSecondDegreeBorders() {
-        for (Country c : this) {
+        for (Country c : countries) {
             c.calculateSecondDegreeBorders();
         }
     }
 
 
     public Country getCountry(String nameOfCountry) {
-        for (Country country : this) {
+        for (Country country : countries) {
             if (country.getName() == nameOfCountry) {
                 return country;
             }
@@ -27,10 +29,21 @@ public class Map extends ArrayList<Country> {
 
     public String toString() {
         String temp = new String();
-        for (Country c : this) {
+        for (Country c : countries) {
             temp += "Coutnry: " + c + "\n\t Borders: " + c.getBorders().toString() + "\n\n";
         }
 
         return temp;
+    }
+
+    public void verifyBorders() {
+        for (Country country : countries) {
+            for (Country borderCountry : country.getBorders()) {
+                if (!borderCountry.contains(country)) {
+                    System.out.println("Country of issue: " + borderCountry + " does not contain " + country);
+                }
+
+            }
+        }
     }
 }
