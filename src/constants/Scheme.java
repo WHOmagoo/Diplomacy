9a7f103c7a1c16cee5a72ec8deb71d33be205fe7
Hugh McGough <hrmagoo@gmail.com>
2015-06-03 11:56:02 -0700

    Re added the mysteriously deleted constants folder

---
+++ src/constants/Scheme.java
@@ -0,0 +1,26 @@
+package constants;
+
+import java.awt.*;
+
+public enum Scheme {
+    FOREGROUND (Color.BLUE), BACKGROUND(Color.ORANGE), FONT(new Font("Dialog", Font.BOLD, 12));
+
+    Color color;
+    Font font;
+
+    Scheme(Color color) {
+        this.color = color;
+    }
+
+    Scheme(Font font){
+        this.font = font;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public Font getFont() {
+        return font;
+    }
+}
